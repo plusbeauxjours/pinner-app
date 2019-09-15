b376823c27092ab9c0bae84369bf45223281a23c
plusbeauxjours <plusbeauxjours@gmail.com>
2019-09-16 00:14:30 +0900

    authHome

--- src/components/NavController.tsx
+++ src/components/NavController.tsx
@@ -5,7 +5,8 @@ import AuthNavigation from "../navigation/AuthNavigation";
 import MainNavigation from "../navigation/MainNavigation";
 
 export default () => {
-  const isLoggedIn = useIsLoggedIn();
+  // const isLoggedIn = useIsLoggedIn();
+  const isLoggedIn = false;
   return (
     <View style={{ flex: 1 }}>
       {isLoggedIn ? <MainNavigation /> : <AuthNavigation />}
