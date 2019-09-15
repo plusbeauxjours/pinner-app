e2287124937c7c284c561936d28c274637290004
plusbeauxjours <plusbeauxjours@gmail.com>
2019-09-15 17:19:46 +0900

    messagesNavigation 2/2

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,9 +1,10 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import TabNavigation from "./TabNavigation";
 import PhotoNavigation from "./PhotoNavigation";
+import MessageNavigation from "./MessageNavigation";
 
 const MainNavigation = createStackNavigator(
-  { TabNavigation, PhotoNavigation },
+  { TabNavigation, PhotoNavigation, MessageNavigation },
   { headerMode: "none", mode: "modal" }
 );
 
