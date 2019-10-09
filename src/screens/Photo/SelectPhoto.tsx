0532825f203ba435692a18c8337f5cf311cf5318
plusbeauxjours <plusbeauxjours@gmail.com>
2019-10-09 18:03:32 +0900

    selectPhoto 1/2

--- src/screens/Photo/SelectPhoto.tsx
+++ src/screens/Photo/SelectPhoto.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import * as Permissions from "expo-permissions";
+import * as MediaLibrary from "expo-media-library";
+import Loader from "../../components/Loader";
+import { Image } from "react-native";
 
 const View = styled.View`
   background-color: ${props => props.theme.bgColor};
@@ -12,8 +16,52 @@ const Text = styled.Text`
   color: ${props => props.theme.color};
 `;
 
-export default () => (
-  <View>
-    <Text>Select</Text>
-  </View>
-);
+export default () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const [selected, setSelected] = useState();
+  const [allPhotos, setAllPhotos] = useState();
+  const getPhotos = async () => {
+    try {
+      const { assets } = await MediaLibrary.getAssetsAsync();
+      const [firstPhoto] = assets;
+      setSelected(firstPhoto);
+      setAllPhotos(assets);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const askPermission = async () => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status === "granted") {
+        setHasPermission(true);
+        getPhotos();
+      }
+    } catch (e) {
+      console.log(e);
+      setHasPermission(false);
+    }
+  };
+  useEffect(() => {
+    askPermission();
+  }, []);
+  return (
+    <View>
+      {loading ? (
+        <Loader />
+      ) : (
+        <View>
+          {hasPermission ? (
+            <Image
+              style={{ width: 100, height: 100 }}
+              source={{ uri: selected.uri }}
+            />
+          ) : null}
+        </View>
+      )}
+    </View>
+  );
+};
